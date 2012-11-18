4c43d4afb2b7a8078fdc274f6a34e7717e099b14
John Doe <allyourcoolnamesarebelongtous@gmail.com>
2012-11-18 16:08:01 +0000

    First Release

---
+++ checker.lua
@@ -0,0 +1,36 @@
+checkers={}
+
+function conforms(t,a)
+	return t == "?"
+	or (t:sub(1, 1) == "?" and (a==nil or conforms(t:sub(2, -1),a)))
+	or type(a) == t
+	or (pcall(getmetatable,a) and getmetatable(a) and getmetatable(a).__type == t)
+	or (checkers[t] and checkers[t](a))
+end
+
+function check_one(d,a)
+	for t in d:gmatch('|?([^|]*)|?') do
+		if conforms(t,a) then return true end
+	end
+	return false
+end
+
+function checka(...)
+	for i=1,select('#',...),2 do
+		if not check_one(select(i,...)) then
+			error('arg number '..i..':'..tostring(select(i+1,...),nil)..'of type '..tostring(type(select(i+1,...)))..' is not of type '..tostring(select(i,...),nil),3)
+		end
+	end
+end
+function checkb(...)
+	local n=select('#',...)
+	for i=1,n/2 do
+		if not check_one(select(i,...),select(n-i+1,...)) then
+			error('args number '..i..':'..tostring(select(n-i+1,...),nil)..' of type '..tostring(type(select(n-i+1,...)))..' is not of type'..tostring(select(i,...),nil),3)
+		end
+	end
+end
+
+check=checkb
+return check
+--packages/checker.lua
